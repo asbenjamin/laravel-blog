c8914f0d27960141c738a79b15ee0f81abe00a15
asbenjamin <shammahbenjamin@gmail.com>
2023-07-07 08:53:20 +0300

    Laravel blog

---
+++ app/Models/Post.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $with = ['category', 'author'];
+
+    // queryscopes
+    // public function scopeFilter($query, array $filters) // allows Post::newQuery()->filter()
+    // {
+    //     if ($filters['search'] ?? false){
+    //         $query
+    //             ->where('title', 'like', '%' . request('search') . '%')
+    //             ->orWhere('body', 'like', '%' . request('search') . '%');
+    //     }
+    // }
+
+    // with query builder
+    public function scopeFilter ($query, array $filters)
+    {
+        // note the arrow function, read more about this query->when thing, how the $search comes in?
+        $query->when($filters['search'] ?? false, fn ($query, $search) => 
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+            );
+    }
+
+
+
+    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        $user = User::factory()->create([
+            'username' => 'shamben'
+        ]);
+
+        Post::factory(20)->create([
+            'user_id' => $user->first()->id,
+            // 'category_id' =>1
+        ]);
+
+    }
+}
